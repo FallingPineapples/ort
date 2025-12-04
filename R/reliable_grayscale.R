7baca41f13f747c5cd70250126b6994400165179
N Brown <natezbrown@gmail.com>
2025-12-03 18:11:35 -0800

    add fixed colorspace conversion wrappers

--- R/reliable_grayscale.R
+++ R/reliable_grayscale.R
@@ -4,18 +4,14 @@ ensure_grayscale = function(img, bg=1) {
   } else if (spectrum(img) == 3) {
     # imager::grayscale XYZ method with no XYZtoRGB
     # should be idempotent
-    Y = img %>% sRGBtoRGB %>% RGBtoXYZ %>% G
-    out = Y %>% add.colour %>% RGBtosRGB %>% R
+    Y = img %>% .sRGBtoRGB %>% RGBtoXYZ %>% G
+    out = Y %>% add.colour %>% .RGBtosRGB %>% R
     return(out)
   } else if (spectrum(img) == 4) {
-    Y = img[,,,1:3,drop=FALSE] %>% sRGBtoRGB %>% RGBtoXYZ %>% G
-    out = lerp(bg, Y, img[,,,4,drop=FALSE]) %>% add.colour %>% RGBtosRGB %>% R
+    Y = img[,,,1:3,drop=FALSE] %>% .sRGBtoRGB %>% RGBtoXYZ %>% G
+    out = lerp(bg, Y, img[,,,4,drop=FALSE]) %>% add.colour %>% .RGBtosRGB %>% R
     return(out)
   } else {
     # TODO: complain
   }
-}
-
-lerp = function(a, b, t) {
-  a + (b - a) * t
 }
---
+++ R/utils.R
@@ -0,0 +1,25 @@
+lerp = function(a, b, t) {
+  a + (b - a) * t
+}
+
+.pkg_env <- new.env(parent = emptyenv())
+.onLoad = function(libname, pkgname) {
+  im <- structure(c(1, 1, 1), dim = c(1L, 1L, 1L, 3L), class = c("cimg", "imager_array", "numeric"))
+  .pkg_env$is_fixed = isTRUE(all.equal(im %>% RGBtosRGB, im, tolerance=1e-6))
+}
+
+.RGBtosRGB = function (im) {
+  if (.pkg_env$is_fixed) {
+    imager::RGBtosRGB(im)
+  } else {
+    imager::RGBtosRGB(im*255)/255
+  }
+}
+
+.sRGBtoRGB = function (im) {
+  if (.pkg_env$is_fixed) {
+    imager::sRGBtoRGB(im)
+  } else {
+    imager::sRGBtoRGB(im*255)/255
+  }
+}
