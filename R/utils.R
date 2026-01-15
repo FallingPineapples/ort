b79f23e44c97660d2626e41f1e34086c14b6837d
Ethan Brown <ethancbrown@gmail.com>
2026-01-15 09:05:28 +0700

    .onLoad only uses base

--- R/utils.R
+++ R/utils.R
@@ -5,7 +5,7 @@ lerp = function(a, b, t) {
 .pkg_env <- new.env(parent = emptyenv())
 .onLoad = function(libname, pkgname) {
   im <- structure(c(1, 1, 1), dim = c(1L, 1L, 1L, 3L), class = c("cimg", "imager_array", "numeric"))
-  .pkg_env$is_fixed = isTRUE(all.equal(im %>% RGBtosRGB, im, tolerance=1e-6))
+  .pkg_env$is_fixed = isTRUE(all.equal(RGBtosRGB(im), im, tolerance=1e-6))
 }
 
 .RGBtosRGB = function (im) {
